e1c56a0a78533f9c72f1deeb586a00206caa674d
leonardo caixeta <leonardocaixeta@gmail.com>
2023-07-06 15:35:06 -0300

    requisito 1

--- src/components/TableOfPlanets.jsx
+++ src/components/TableOfPlanets.jsx
@@ -1,16 +1,53 @@
 import { useEffect, useState } from 'react';
-import apiFetch from '../funcs/Fetch';
 
 function TableOfPlanets() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState();
 
   useEffect(() => {
-    const apiData = apiFetch();
-    setData(apiData);
+    fetch('https://swapi.dev/api/planets')
+      .then((api) => api.json())
+      .then(({ results }) => setData(results));
   }, []);
 
   return (
-    <p>nada</p>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Climate</th>
+          <th>Terrain</th>
+          <th>Population</th>
+          <th>Diameter</th>
+          <th>Gravity</th>
+          <th>Orbital Period</th>
+          <th>Rotation Period</th>
+          <th>Surface Water</th>
+          <th>Films</th>
+          <th>Created</th>
+          <th>Edited</th>
+          <th>URL</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data && data.map((el, index) => (
+          <tr key={ index }>
+            <td>{el.name}</td>
+            <td>{el.climate}</td>
+            <td>{el.terrain}</td>
+            <td>{el.population}</td>
+            <td>{el.diameter}</td>
+            <td>{el.gravity}</td>
+            <td>{el.orbital_period}</td>
+            <td>{el.rotation_period}</td>
+            <td>{el.surface_water}</td>
+            <td>{el.films.join(', ')}</td>
+            <td>{el.created}</td>
+            <td>{el.edited}</td>
+            <td>{el.url}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
 
--- src/funcs/Fetch.js
+++
@@ -1,7 +0,0 @@
-export default async function apiFetch() {
-  const api = await fetch('https://swapi.dev/api/planets');
-  const response = await api.json();
-  const res = await response.results;
-  console.log();
-  return res;
-}
