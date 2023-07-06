7658f25637a985df6403c12f224a9383eb77739f
leonardo caixeta <leonardocaixeta@gmail.com>
2023-07-06 15:51:44 -0300

    requisito 2

--- src/components/TableOfPlanets.jsx
+++ src/components/TableOfPlanets.jsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function TableOfPlanets() {
   const [data, setData] = useState();
+  const [filterText, setFilterText] = useState('');
 
   useEffect(() => {
     fetch('https://swapi.dev/api/planets')
@@ -9,45 +10,65 @@ function TableOfPlanets() {
       .then(({ results }) => setData(results));
   }, []);
 
+  const handleOnChange = ({ target }) => {
+    setFilterText(target.value);
+  };
+
+  const filteredData = data && data.filter((el) => el.name.toLowerCase()
+    .includes(filterText.toLowerCase()));
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Climate</th>
-          <th>Terrain</th>
-          <th>Population</th>
-          <th>Diameter</th>
-          <th>Gravity</th>
-          <th>Orbital Period</th>
-          <th>Rotation Period</th>
-          <th>Surface Water</th>
-          <th>Films</th>
-          <th>Created</th>
-          <th>Edited</th>
-          <th>URL</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data && data.map((el, index) => (
-          <tr key={ index }>
-            <td>{el.name}</td>
-            <td>{el.climate}</td>
-            <td>{el.terrain}</td>
-            <td>{el.population}</td>
-            <td>{el.diameter}</td>
-            <td>{el.gravity}</td>
-            <td>{el.orbital_period}</td>
-            <td>{el.rotation_period}</td>
-            <td>{el.surface_water}</td>
-            <td>{el.films.join(', ')}</td>
-            <td>{el.created}</td>
-            <td>{el.edited}</td>
-            <td>{el.url}</td>
+    <>
+      <label htmlFor="name-filter">
+        <input
+          type="text"
+          id="name-filter"
+          data-testid="name-filter"
+          onChange={ handleOnChange }
+          value={ filterText }
+        />
+
+      </label>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Climate</th>
+            <th>Terrain</th>
+            <th>Population</th>
+            <th>Diameter</th>
+            <th>Gravity</th>
+            <th>Orbital Period</th>
+            <th>Rotation Period</th>
+            <th>Surface Water</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>URL</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {filteredData && filteredData.map((el, index) => (
+            <tr key={ index }>
+              <td>{el.name}</td>
+              <td>{el.climate}</td>
+              <td>{el.terrain}</td>
+              <td>{el.population}</td>
+              <td>{el.diameter}</td>
+              <td>{el.gravity}</td>
+              <td>{el.orbital_period}</td>
+              <td>{el.rotation_period}</td>
+              <td>{el.surface_water}</td>
+              <td>{el.films.join(', ')}</td>
+              <td>{el.created}</td>
+              <td>{el.edited}</td>
+              <td>{el.url}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+
   );
 }
 
